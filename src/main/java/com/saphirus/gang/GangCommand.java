73a77ca4d4ee3b6dbc4b07132833cd1644e87adc
Veyzn <officialluzex@gmail.com>
2024-06-20 16:32:28 +0200

    Continued Gang Command

--- src/main/java/com/saphirus/gang/GangCommand.java
+++ src/main/java/com/saphirus/gang/GangCommand.java
@@ -2,6 +2,7 @@ package com.saphirus.gang;
 
 import com.saphirus.main.Data;
 import com.saphirus.main.Main;
+import com.saphirus.utils.Maths;
 import com.saphirus.utils.TempPlayerCache;
 import com.saphirus.utils.Utils;
 import net.md_5.bungee.api.chat.ClickEvent;
@@ -40,6 +41,7 @@ public class GangCommand implements CommandExecutor, TabCompleter {
         if(tpc.inTeam()) {
             gm = new GangManager(tpc.getTeam());
         }
+        GangManager finalGm = gm;
         switch (args.length) {
             default -> sendHelp(p);
 
@@ -86,10 +88,9 @@ public class GangCommand implements CommandExecutor, TabCompleter {
                     case "stats" -> {
                         if(tpc.inTeam()) {
                             assert gm != null;
-                            //RANKING HAS TO BE DONE
                             p.sendMessage("                   §a§l" + gm.getName() + " GANG");
                             p.sendMessage("");
-                            p.sendMessage(Data.Gang + "Name: §a" + gm.getName() + " §8(§e#1§8) §8| §fTrophies: §e" + gm.getTrophies());
+                            p.sendMessage(Data.Gang + "Name: §a" + gm.getName() + " §8(§e#" + gm.getGangPlacement() + "§8) §8| §fTrophies: §e" + gm.getTrophies());
                             p.sendMessage(Data.Gang + "Owner: " + Utils.OnOffTest(UUID.fromString(gm.getOwner())) + " §8| §fLevel: §a" + gm.getTeamLevel());
                             p.sendMessage(Data.Gang + "Balance: §a$" + Utils.getZahl(gm.getBankMoney()) + " §8| §fGems: §2" + Utils.getZahl(gm.getBankGems()));
                             p.sendMessage(Data.Gang);
@@ -109,8 +110,12 @@ public class GangCommand implements CommandExecutor, TabCompleter {
 
                     }
 
-                    case "trophy" -> {
-                        //HAS TO BE DONE
+                    case "trophy", "trophies" -> {
+                        if(tpc.inTeam()) {
+                            if(isOwner(gm,p.getUniqueId().toString())) {
+                                gm.openTrophyInventory(p);
+                            } else sendNotOwner(p);
+                        } else sendNotInGang(p);
                     }
 
                     case "level" -> {
@@ -160,11 +165,11 @@ public class GangCommand implements CommandExecutor, TabCompleter {
                                 if(Bukkit.getPlayer(args[1]) != null) {
                                     Player t = Bukkit.getPlayer(args[1]);
                                     TempPlayerCache ttc = new TempPlayerCache(t.getUniqueId().toString());
-                                    if(gm.getMembers().size() > 4) {
+                                    if(gm.getMembers().size() < 4) {
                                         if(!ttc.inTeam()) {
-                                            if(!invited.containsValue(t.getUniqueId().toString()) || !invited.get(t.getUniqueId().toString()).contains(gm.getName())) {
-
-                                                TextComponent message = new TextComponent(Data.Gang + "The gang §c " + gm.getName() + " §finvited you to! Click");
+                                            if(!invited.containsKey(t.getUniqueId().toString()) || !invited.get(t.getUniqueId().toString()).contains(gm.getName())) {
+                                                t.sendMessage(Data.Gang + "The gang §c" + gm.getName() + " §finvited you!");
+                                                TextComponent message = new TextComponent("             Click ");
 
                                                 // Create the clickable part
                                                 TextComponent clickablePart = new TextComponent("§a§lHERE");
@@ -175,6 +180,27 @@ public class GangCommand implements CommandExecutor, TabCompleter {
                                                 message.addExtra(" §fto join the gang");
 
                                                 t.spigot().sendMessage(message);
+                                                p.sendMessage(Data.Gang + "You have invited §c" + t.getName() + " §fto the gang!");
+
+                                                if(!invited.containsValue(t.getUniqueId().toString())) {
+                                                    ArrayList<String> new_invited = new ArrayList<>();
+                                                    new_invited.add(gm.getName());
+                                                    invited.put(t.getUniqueId().toString(), new_invited);
+                                                } else {
+                                                    ArrayList<String> invites = invited.get(t.getUniqueId());
+                                                    invites.add(gm.getName());
+                                                    invited.put(t.getUniqueId().toString(), invites);
+                                                }
+
+
+                                                new BukkitRunnable() {
+                                                    @Override
+                                                    public void run() {
+                                                        ArrayList<String> invites = invited.get(t.getUniqueId().toString());
+                                                        invites.remove(finalGm.getName());
+                                                        invited.put(t.getUniqueId().toString(), invites);
+                                                    }
+                                                }.runTaskLaterAsynchronously(Main.instance,20*30);
 
                                             } else p.sendMessage(Data.Gang + "You already invited this player!");
                                         } else p.sendMessage(Data.Gang + "This player is already in a gang!");
@@ -185,7 +211,133 @@ public class GangCommand implements CommandExecutor, TabCompleter {
                     }
 
                     case "join" -> {
+                        if(tpc.inTeam()) {
+                            p.sendMessage(Data.Gang + "You are already in a gang");
+                            return true;
+                        }
 
+                        GangManager join_gm = new GangManager(args[1]);
+
+                        if(!invited.containsKey(p.getUniqueId().toString()) || !invited.get(p.getUniqueId().toString()).contains(join_gm.getName())) {
+                            p.sendMessage(Data.Gang + "The gang did not invite you!");
+                            return true;
+                        }
+
+                        if(!join_gm.exists()) {
+                            p.sendMessage(Data.Gang + "That gang does not exist!");
+                            return true;
+                        }
+
+                        if(join_gm.getMembers().size() == 4) {
+                            p.sendMessage(Data.Gang + "The gang has already reached the maximum of 4 members");
+                            return true;
+                        }
+
+                        tpc.setTeam(join_gm.getName());
+                        join_gm.addMember(p.getUniqueId().toString());
+                        join_gm.sendGangMessage("The player §c" + p.getName() + "§a joined the gang!");
+
+                        ArrayList<String> invites = invited.get(p.getUniqueId().toString());
+                        invites.remove(join_gm.getName());
+                        invited.put(p.getUniqueId().toString(), invites);
+
+                    }
+
+                    case "stats" -> {
+                        GangManager gm_stats = new GangManager(args[1]);
+                        if(gm_stats.exists()) {
+                            p.sendMessage("                   §a§l" + gm_stats.getName() + " GANG");
+                            p.sendMessage("");
+                            p.sendMessage(Data.Gang + "Name: §a" + gm_stats.getName() + " §8(§e#" + gm_stats.getGangPlacement() + "§8) §8| §fTrophies: §e" + gm_stats.getTrophies());
+                            p.sendMessage(Data.Gang + "Owner: " + Utils.OnOffTest(UUID.fromString(gm_stats.getOwner())) + " §8| §fLevel: §a" + gm_stats.getTeamLevel());
+                            p.sendMessage(Data.Gang);
+                            p.sendMessage(Data.Gang + "Members");
+                            String members = "";
+                            for (int i = 0; i < gm_stats.getMembers().size(); i++) {
+                                if (i != gm_stats.getMembers().size()) {
+                                    members += Utils.OnOffTest(UUID.fromString(gm_stats.getMembers().get(i))) + " §8• ";
+                                } else members += Utils.OnOffTest(UUID.fromString(gm_stats.getMembers().get(i)));
+                            }
+                            if (!gm_stats.getMembers().isEmpty()) {
+                                p.sendMessage(Data.Gang + members);
+                            } else p.sendMessage(Data.Gang + "§aThis gang has no members");
+
+                            p.sendMessage("");
+                        } else p.sendMessage(Data.Gang + "That gang does not exist!");
+                    }
+                }
+            }
+
+            case 3 -> {
+                switch (args[0].toLowerCase()) {
+                    default -> sendHelp(p);
+
+                    case "deposit" -> {
+                        if(!tpc.inTeam()) {
+                            sendNotInGang(p);
+                            return true;
+                        }
+
+                        if(!Maths.isLong(args[2])) {
+                            p.sendMessage(Data.Gang + "This is an invalid number!");
+                            return true;
+                        }
+
+                        Long amount = Long.parseLong(args[2]);
+
+                        if(args[1].equalsIgnoreCase("money")) {
+                            if(tpc.getMoney() < amount) {
+                                p.sendMessage(Data.Gang + "You don't have enough money!");
+                                return true;
+                            }
+
+                            gm.setBankMoney(gm.getBankMoney() + amount);
+                            tpc.removeMoney(amount);
+                            gm.sendGangMessage("§c" + p.getName() + " §adeposited §c$" + Utils.getZahl(amount) + " §ato the bank!");
+
+                        } else if(args[1].equalsIgnoreCase("gems")) {
+                            if(tpc.getGems() < amount) {
+                                p.sendMessage(Data.Gang + "You don't have enough gems!");
+                                return true;
+                            }
+
+                            gm.setBankGems(gm.getBankGems() + amount);
+                            tpc.removeGems(amount);
+                            gm.sendGangMessage("§c" + p.getName() + " §adeposited §c" + Utils.getZahl(amount) + " Gems §ato the bank!");
+                        } else sendHelp(p);
+
+                    }
+
+                    case "withdraw" -> {
+                        if(!tpc.inTeam()) {
+                            sendNotInGang(p);
+                            return true;
+                        }
+
+                        if(!Maths.isLong(args[2])) {
+                            p.sendMessage(Data.Gang + "This is an invalid number!");
+                            return true;
+                        }
+
+                        Long amount = Long.parseLong(args[2]);
+
+                        if(args[1].equalsIgnoreCase("money")) {
+                            if(gm.getBankMoney() < amount) {
+                                p.sendMessage(Data.Gang + "Your gang does not have enough money in the bank!");
+                                return true;
+                            }
+                            gm.setBankMoney(gm.getBankMoney() - amount);
+                            tpc.addMoney(amount);
+                            gm.sendGangMessage("§c" + p.getName() + " §awithdrew §c$" + Utils.getZahl(amount) + " §afrom the bank!");
+                        } else if(args[1].equalsIgnoreCase("gems")) {
+                            if(gm.getBankGems() < amount) {
+                                p.sendMessage(Data.Gang + "Your gang does not have enough gems in the bank!");
+                                return true;
+                            }
+                            gm.setBankGems(gm.getBankGems() - amount);
+                            tpc.addGems(amount);
+                            gm.sendGangMessage("§c" + p.getName() + " §awithdrew §c" + Utils.getZahl(amount) + " Gems §afrom the bank!");
+                        } else sendHelp(p);
                     }
                 }
             }
@@ -202,9 +354,9 @@ public class GangCommand implements CommandExecutor, TabCompleter {
         default_completer.add("create"); //DONE
         default_completer.add("disband"); //DONE
         default_completer.add("kick"); //DONE
-        default_completer.add("invite");
-        default_completer.add("kick");
-        default_completer.add("join");
+        default_completer.add("invite");//DONE
+        default_completer.add("kick"); //DONE
+        default_completer.add("join"); //DONE
         default_completer.add("leave"); //DONE
         default_completer.add("stats"); //DONE
         default_completer.add("deposit");
@@ -212,10 +364,36 @@ public class GangCommand implements CommandExecutor, TabCompleter {
         default_completer.add("trophies");
         default_completer.add("level");
 
+        List gangs = new ArrayList<String>();
+        for(GangManager gm : GangManager.getAllGangs()) {
+            gangs.add(gm.getName());
+        }
         List<String> completer = new ArrayList<>();
         switch (args.length) {
             case 1 -> {
                 return default_completer;
+            }
+
+            case 2 -> {
+                if(args[0].equalsIgnoreCase("stats") || args[0].equalsIgnoreCase("join")) {
+
+                    return gangs;
+                }
+
+                if(args[0].equalsIgnoreCase("withdraw") || args[0].equalsIgnoreCase("deposit") ) {
+                    return List.of("gems", "money");
+                }
+
+                if(args[0].equalsIgnoreCase("create")) {
+                    return List.of("Your gang name");
+                }
+
+            }
+
+            case 3 -> {
+                if(args[0].equalsIgnoreCase("withdraw") || args[0].equalsIgnoreCase("deposit")) {
+                    return List.of("amount");
+                }
             }
         }
 
--- src/main/java/com/saphirus/gang/GangManager.java
+++ src/main/java/com/saphirus/gang/GangManager.java
@@ -1,29 +1,36 @@
 package com.saphirus.gang;
 
+import com.saphirus.fastinv.FastInv;
+import com.saphirus.fastinv.ItemBuilder;
 import com.saphirus.main.Data;
 import com.saphirus.utils.TempPlayerCache;
+import com.saphirus.utils.Utils;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class GangManager {
 
     private String name;
     private File file;
     private YamlConfiguration cfg;
+    private static final File GANGS_FOLDER = new File("plugins/Saphirus/Gangs");
 
     public GangManager(String names) {
         name = names;
-        file = new File("plugins/Saphirus/Gangs/" + name.toLowerCase() + ".yml");
+        file = new File(GANGS_FOLDER, name.toLowerCase() + ".yml");
         cfg = YamlConfiguration.loadConfiguration(file);
     }
-
 
     public boolean exists(){return file.exists();}
 
@@ -34,10 +41,10 @@ public class GangManager {
             cfg.set("Name", name);
             cfg.set("Owner", uuid_owner);
             cfg.set("Trophies", 0L);
-            cfg.set("Members", new ArrayList<String>());
+            cfg.set("Members", new ArrayList<>());
             cfg.set("Bank.money", 0L);
             cfg.set("Bank.gems", 0L);
-            cfg.set("Team-Level",0);
+            cfg.set("Team-Level", 0);
 
             saveConfig();
         } catch (IOException e) {
@@ -65,7 +72,6 @@ public class GangManager {
         saveConfig();
     }
 
-
     public String getOwner() {
         return cfg.getString("Owner");
     }
@@ -74,6 +80,7 @@ public class GangManager {
         cfg.set("Owner", uuid_owner);
         saveConfig();
     }
+
     public long getTrophies() {
         return cfg.getLong("Trophies");
     }
@@ -82,7 +89,6 @@ public class GangManager {
         cfg.set("Trophies", trophies);
         saveConfig();
     }
-
 
     public List<String> getMembers() {
         return cfg.getStringList("Members");
@@ -102,7 +108,6 @@ public class GangManager {
         saveConfig();
     }
 
-
     public long getBankMoney() {
         return cfg.getLong("Bank.money");
     }
@@ -112,7 +117,6 @@ public class GangManager {
         saveConfig();
     }
 
-
     public long getBankGems() {
         return cfg.getLong("Bank.gems");
     }
@@ -121,7 +125,6 @@ public class GangManager {
         cfg.set("Bank.gems", gems);
         saveConfig();
     }
-
 
     public int getTeamLevel() {
         return cfg.getInt("Team-Level");
@@ -163,5 +166,130 @@ public class GangManager {
                 every.sendMessage(Data.Gang + "§a" + message);
             }
         }
+    }
+
+    public static List<GangManager> getAllGangs() {
+        List<GangManager> gangs = new ArrayList<>();
+        File[] files = GANGS_FOLDER.listFiles((dir, name) -> name.endsWith(".yml"));
+        if (files != null) {
+            for (File file : files) {
+                String gangName = file.getName().replace(".yml", "");
+                GangManager gm = new GangManager(gangName);
+                if (gm.exists()) {
+                    gangs.add(gm);
+                }
+            }
+        }
+        return gangs;
+    }
+
+    public static List<GangManager> getTop10Gangs() {
+        return getAllGangs().stream()
+                .sorted(Comparator.comparingLong(GangManager::getTrophies).reversed())
+                .limit(10)
+                .collect(Collectors.toList());
+    }
+
+    public int getGangPlacement() {
+        List<GangManager> sortedGangs = getAllGangs().stream()
+                .sorted(Comparator.comparingLong(GangManager::getTrophies).reversed())
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < sortedGangs.size(); i++) {
+            if (sortedGangs.get(i).getName().equalsIgnoreCase(getName())) {
+                return i + 1; // 1-based index
+            }
+        }
+        return -1; // Gang not found
+    }
+
+    public void openTrophyInventory(Player p) {
+        FastInv inv = new FastInv(6*9, "§eBuy trophies");
+
+        for(int i : inv.getBorders()) {
+            inv.setItem(i, new ItemBuilder(Material.BLACK_STAINED_GLASS_PANE).name("§1").build());
+        }
+        AtomicInteger amount = new AtomicInteger(1);
+        long price_per_trophy = 5000000L;
+
+        inv.setItem(13, new ItemBuilder(Material.TOTEM_OF_UNDYING)
+                .name("§e§lTROPHIES")
+                .lore("§7This is used to place in the " , "§eTrophy Leaderboard", "","§a$5.000.000 §7per §eTrophy")
+                .build());
+
+        inv.setItem(29, new ItemBuilder(Material.GREEN_STAINED_GLASS_PANE)
+                .name("§a+1")
+                .build(), e -> {
+            amount.getAndAdd(1);
+            updateTrophie(p, amount,inv,price_per_trophy);
+        });
+
+        inv.setItem(30, new ItemBuilder(Material.GREEN_STAINED_GLASS_PANE)
+                .name("§a+10")
+                .build(), e -> {
+            amount.getAndAdd(10);
+            updateTrophie(p, amount,inv,price_per_trophy);
+        });
+
+        inv.setItem(31, new ItemBuilder(Material.GREEN_STAINED_GLASS_PANE)
+                .name("§a+50")
+                .build(), e -> {
+            amount.getAndAdd(50);
+            updateTrophie(p, amount,inv,price_per_trophy);
+        });
+
+        inv.setItem(32, new ItemBuilder(Material.GREEN_STAINED_GLASS_PANE)
+                .name("§a+100")
+                .build(), e -> {
+            amount.getAndAdd(100);
+            updateTrophie(p, amount,inv,price_per_trophy);
+        });
+
+        inv.setItem(33, new ItemBuilder(Material.GREEN_STAINED_GLASS_PANE)
+                .name("§a§lMAX §8(§e" + Utils.getZahl(getBankMoney()/price_per_trophy) + " Trophies§8)")
+                .build(), e -> {
+            amount.getAndSet((int) (getBankMoney()/price_per_trophy));
+            updateTrophie(p, amount,inv,price_per_trophy);
+        });
+
+        inv.setItem(40, new ItemBuilder(getBankMoney() >= price_per_trophy*amount.get()  && amount.get() >0? Material.LIME_DYE:Material.RED_DYE)
+                .name(getBankMoney() >= price_per_trophy*amount.get() ? "§aConfirm purchase":"§cNot enough money")
+                .lore("§7Amount: §e" + amount.get() + " Trophies", "§7Cost: §a$" + Utils.getZahl(price_per_trophy * amount.get()))
+                .build(), e -> {
+                    if(getBankMoney() >= price_per_trophy * amount.get()) {
+                        setTrophies(getTrophies() + amount.get());
+                        setBankMoney(getBankMoney() - (price_per_trophy*amount.get()));
+                        sendGangMessage("§c" + p.getName() + " §apurchased §e" + amount.get() + " Trophies §afor $" + Utils.getZahl(price_per_trophy*amount.get()));
+                        p.closeInventory();
+                    }
+
+        });
+
+        inv.open(p);
+
+
+    }
+
+    public void updateTrophie(Player p, AtomicInteger amount, FastInv inv,long price_per_trophy) {
+        inv.setItem(13, new ItemBuilder(Material.TOTEM_OF_UNDYING)
+                .name("§e§lTROPHIES")
+                .amount(amount.get() == 0 ? 1:amount.get())
+                .lore("§7This is used to place in the " , "§eTrophy Leaderboard", "","§a$5.000.000 §7per §eTrophy")
+                .build());
+
+        inv.setItem(40, new ItemBuilder(getBankMoney() >= price_per_trophy*amount.get() && amount.get() >0 ? Material.LIME_DYE:Material.RED_DYE)
+                .name(getBankMoney() >= price_per_trophy*amount.get() ? "§aConfirm purchase":"§cNot enough money")
+                .lore("§7Amount: §e" + amount.get() + " Trophies", "§7Cost: §a$" + Utils.getZahl(price_per_trophy * amount.get()))
+                .build(), e -> {
+            if(getBankMoney() >= price_per_trophy * amount.get()  && amount.get() >0) {
+                setTrophies(getTrophies() + amount.get());
+                setBankMoney(getBankMoney() - (price_per_trophy*amount.get()));
+                sendGangMessage("§c" + p.getName() + " §apurchased §e" + amount.get() + " Trophies §afor $" + Utils.getZahl(price_per_trophy*amount.get()));
+                p.closeInventory();
+            }
+
+        });
+
+        p.updateInventory();
     }
 }
